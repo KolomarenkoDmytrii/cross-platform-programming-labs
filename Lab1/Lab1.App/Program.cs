a51b04248b7424fb2608bbfa46965d5142267aa0
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-09-21 00:53:44 +0300

    Add tests for Lab1.App.Program.IsDnaSequence()

---
+++ Lab1/Lab1.App/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Lab1.App
+{
+    public class Program
+    {
+        public static bool IsDnaSequence(string sequence)
+        {
+            if (sequence.Length == 0)
+                return false;
+
+            char[] allowedSymbols = ['A', 'G', 'C', 'T'];
+            foreach (char symbol in sequence.ToUpper())
+            {
+                if (!allowedSymbols.Contains(symbol))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsDnaSubsequence(string sequence, string subsequence)
+        {
+            if (subsequence.Length > sequence.Length)
+                return false;
+
+            if (!IsDnaSequence(sequence) || !IsDnaSequence(subsequence))
+                return false;
+
+            int count = 0;
+            foreach (char symbol in sequence)
+            {
+                if (Char.ToUpper(subsequence[count]) == Char.ToUpper(symbol))
+                    count++;
+            }
+
+            return count == subsequence.Length;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(IsDnaSequence("agcta"));
+            Console.WriteLine(IsDnaSequence("agcfta"));
+            Console.WriteLine(IsDnaSubsequence("agcta", "gta"));
+        }
+    }
+}
