82dc5e04d10a55d90a6ceb1bda5767652641f88e
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-10-01 13:47:42 +0300

    Added unit tests for lab work 2

--- Lab2/Lab2.Test/UnitTests.cs
+++ Lab2/Lab2.Test/UnitTests.cs
@@ -1,15 +1,27 @@
+using Lab2.App;
+using System.IO;
+
 namespace Lab2.Test;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    [TestCase(7, 6)]
+    [TestCase(6, 6)]
+    [TestCase(4, 4)]
+    [TestCase(3, 2)]
+    [TestCase(1, 1)]
+    [TestCase(2, 2)]
+    [TestCase(-1, 0)]
+    public void BinarySumsNumberCalculator_isAnswerCorrect(int number, int answer)
     {
-        Assert.Pass();
+        BinarySumsNumberCalculator calculator = new BinarySumsNumberCalculator();
+        int result = calculator.Calculate(number);
+
+        Assert.That(
+            answer == result,
+            Is.True,
+            $"Number {number} must have {answer} binary sum(s), not {result}"
+        );
     }
 }
