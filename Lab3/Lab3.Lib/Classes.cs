1cc06dcc47df9b61af9e6bc10ff09d89c4e34d9e
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-10-03 20:17:35 +0300

    Add lab work 3

---
+++ Lab3/Lab3.Lib/Classes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab3.Lib
+{
+    public class ShortestTimeFinder
+    {
+        public struct Node
+        {
+            public int start;
+            public int end;
+
+            public Node(int start, int end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+        }
+
+        public struct Time
+        {
+            public int start;
+            public int end;
+
+            public Time(int start, int end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+        }
+
+        public struct StationPath
+        {
+            public Node node;
+            public Time time;
+
+            public StationPath(Node node, Time time)
+            {
+                this.node = node;
+                this.time = time;
+            }
+        }
+
+        static public int FindShortestTime(int startStation, int endStation, StationPath[] paths)
+        {
+            int currentStation = startStation;
+            List<int> checkedStations = new List<int>();
+            StationPath minStation = new StationPath();
+            // int answer = -1;
+
+            while (currentStation != endStation)
+            {
+                StationPath[] currentNodes = Array.FindAll(
+                    paths,
+                    station => station.node.start == currentStation
+                );
+
+                Console.WriteLine("------------------------");
+                // foreach (var c in checkedStations)
+                //     Console.WriteLine(c);
+                // Console.WriteLine($"currentStation = {currentStation}");
+                foreach (var c in currentNodes)
+                    Console.WriteLine($"StationPath(Node({c.node.start}, {c.node.end}), Time({c.time.start}, {c.time.end}))");
+
+                if (checkedStations.Contains(currentNodes[0].node.start))
+                    return -1;
+
+                minStation = currentNodes.MinBy(
+                    station => station.time.end - station.time.start
+                );
+                Console.WriteLine($"minStation = StationPath(Node({minStation.node.start}, {minStation.node.end}), Time({minStation.time.start}, {minStation.time.end}))");
+
+                checkedStations.Add(minStation.node.start); //currentStation
+                currentStation = minStation.node.end;
+
+                // Console.WriteLine("=====");
+                // foreach (var c in checkedStations)
+                //     Console.WriteLine(c);
+                // Console.WriteLine($"currentStation = {currentStation}");
+            }
+
+            return minStation.time.end;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -22,3 +22,47 @@ Build a selected project: `dotnet build Build.proj -p:Solution=Lab1 -t:Run`
 Run a selected project: `dotnet build Build.proj -p:Solution=Lab1 -t:Build`
 
 Run test for a selected project: `dotnet build Build.proj -p:Solution=Lab1 -t:Test`
+
+### Lab3
+Lab3.Lib/Lab3.Lib.csproj:
+```
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <Nullable>enable</Nullable>
+
+    <PackageId>DKolomarenko</PackageId>
+    <Version>1.0.0</Version>
+    <Authors>Dmytrii Kolomarenko</Authors>
+  </PropertyGroup>
+
+</Project>
+```
+
+```
+dotnet new sln -o Lab3
+cd Lab3
+
+dotnet new classlib -o Lab3.Lib
+dotnet pack Lab3.Lib/Lab3.Lib.csproj -o packages
+mkdir /home/<user>/.nuget/NuGet/packages
+dotnet nuget push "packages/DKolomarenko.1.0.0.nupkg" -s "/home/<user>/.nuget/NuGet/packages"
+
+dotnet new console -o Lab3.App
+cd Lab3.App
+dotnet add package DKolomarenko
+
+#???
+cd ..
+dotnet new nunit -o Lab3.Test
+dotnet sln ./Lab3.sln add ./Lab3.Test/Lab3.Test.csproj
+dotnet add ./Lab3.Test/Lab3.Test.csproj package DKolomarenko
+dotnet add ./Lab3.Test/Lab3.Test.csproj reference ../Lab3.App/Lab3.App.csproj
+```
+
+Update package:
+1. Update `<Version>1.0.0</Version>`
+2. `dotnet pack Lab3.Lib/Lab3.Lib.csproj -o packages`
+2. `dotnet nuget push "packages/DKolomarenko.1.0.0.nupkg" -s "/home/<user>/.nuget/NuGet/packages"`
