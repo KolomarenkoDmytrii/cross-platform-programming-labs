d78ecdcaa6815f6965356fd4e0a9f37659dce4ce
KolomarenkoDmytrii <dmytrii_kolomarenko@knu.ua>
2024-10-25 13:28:41 +0300

    Implement cli

--- Lab4/Lab4.App/Program.cs
+++ Lab4/Lab4.App/Program.cs
@@ -1,111 +1,199 @@
 ﻿using System;
 using System.IO;
+
 using McMaster.Extensions.CommandLineUtils;
+using DotNetEnv;
+
 using Lab4.Labs;
 
-namespace Lab4.App {
+namespace Lab4.App
+{
     [Command(Name = "run-labs", Description = "Run lab works"),
-     Subcommand(typeof(Run))]
-    class RunLabs {
+     Subcommand(typeof(Run)), Subcommand(typeof(SetPath))]
+    class RunLabs
+    {
         public static int Main(string[] args)
             => CommandLineApplication.Execute<RunLabs>(args);
 
         // [Option(Description = "Lab work to run")]
         // public string Lab { get; }
 
-        [Option(ShortName = "I", Description = "Input file")]
-        public string Input { get; } = "INPUT.TXT";
+        // [Option(ShortName = "I", Description = "Input file")]
+        // public string Input { get; } = "INPUT.TXT";
+        //
+        // [Option(ShortName = "o", Description = "Output file")]
+        // public string Output { get; } = "OUTPUT.TXT";
 
-        [Option(ShortName = "o", Description = "Output file")]
-        public string Output { get; } = "OUTPUT.TXT";
-
-        private int OnExecute(CommandLineApplication app, IConsole console) {
+        private int OnExecute(CommandLineApplication app, IConsole console)
+        {
             console.WriteLine("You must specify at a subcommand.");
             app.ShowHelp();
             return 1;
         }
 
-        // private void OnExecute() {
-        //     // const string labToRun = "lab3";
-        //     // const string inputFilePath = "lab3_input.txt";
-        //     // const string outputFilePath = "lab3_output.txt";
-        //     string labToRun = Lab;
-        //     string inputFilePath = Input;
-        //     string outputFilePath = Output;
-        //
-        //     if (labToRun == "lab1") {
-        //         Lab1.WriteAnswer(inputFilePath, outputFilePath);
-        //     }
-        //     else if (labToRun == "lab2") {
-        //         Lab2.WriteAnswer(inputFilePath, outputFilePath);
-        //     }
-        //     else if (labToRun == "lab3") {
-        //         Lab3.WriteAnswer(inputFilePath, outputFilePath);
-        //     }
-        //
-        //     Console.WriteLine("Hello");
-        //     Console.WriteLine(Lab1.IsDnaSequence("AGCT"));
-        // }
-
         [Command("run", Description = "Run a specified lab work"),
          Subcommand(typeof(RunLab1)),
          Subcommand(typeof(RunLab2)),
          Subcommand(typeof(RunLab3))]
-        private class Run {
+        private class Run
+        {
             private int OnExecute(IConsole console)
             {
                 console.Error.WriteLine("You must specify a lab work to run. See --help for more details.");
                 return 1;
             }
 
-
-
-            [Command("lab3", Description = "Run the lab work 3"),
+            [Command("lab1", Description = "Run the lab work 1"),
              HelpOption,
              Subcommand(typeof(SetPath))]
-            private class RunLab3 {
+            private class RunLab1
+            {
                 [Option(ShortName = "I", Description = "Input file")]
-                public string Input { get; } = "INPUT.TXT";
+                public string Input { get; }
 
                 [Option(ShortName = "o", Description = "Output file")]
-                public string Output { get; } = "OUTPUT.TXT";
+                public string Output { get; }
 
                 private void OnExecute()
                 {
                     try
                     {
-                        Lab3.WriteAnswer(Input, Output);
+                        if (!string.IsNullOrEmpty(Input) && !string.IsNullOrEmpty(Output))
+                        {
+                            Lab1.WriteAnswer(Input, Output);
+                        }
+                        else
+                        {
+                            const string inputFile = "INPUT.TXT";
+                            const string outputFile = "OUTPUT.TXT";
+
+                            string dataDirectory = Environment.GetEnvironmentVariable("LAB_PATH");
+                            if (string.IsNullOrEmpty(dataDirectory))
+                            {
+                                Env.Load(".env");
+                                dataDirectory = Environment.GetEnvironmentVariable("LAB_PATH");
+
+                                if (string.IsNullOrEmpty(dataDirectory))
+                                    dataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                            }
+
+                            string inputPath = Path.Join(dataDirectory, inputFile);
+                            string outputPath = Path.Join(dataDirectory, outputFile);
+                            Lab1.WriteAnswer(inputPath, outputPath);
+                        }
                     }
                     catch (Exception exception)
                     {
                         Console.Error.WriteLine($"An error occured: {exception}");
                     }
                 }
+            }
 
-                [Command("set-path", Description = "Run the lab work 3"), HelpOption]
-                private class SetPath {
-                    [Option("-p|--path", Description = "Input and output files directory")]
-                    public string ResultPath { get; set; }
+            [Command("lab2", Description = "Run the lab work 2"),
+             HelpOption,
+             Subcommand(typeof(SetPath))]
+            private class RunLab2
+            {
+                [Option(ShortName = "I", Description = "Input file")]
+                public string Input { get; }
 
-                    private void OnExecute()
+                [Option(ShortName = "o", Description = "Output file")]
+                public string Output { get; }
+
+                private void OnExecute()
+                {
+                    try
                     {
-                        try
+                        if (!string.IsNullOrEmpty(Input) && !string.IsNullOrEmpty(Output))
                         {
-                            if (string.IsNullOrEmpty(ResultPath)) {
-                                ResultPath = Environment.GetFolderPath(
-                                    Environment.SpecialFolder.UserProfile
-                                );
-                            }
-                            Lab3.WriteAnswer(
-                                Path.Join(ResultPath, "INPUT.TXT"), Path.Join(ResultPath, "OUTPUT.TXT")
-                            );
+                            Lab2.WriteAnswer(Input, Output);
                         }
-                        catch (Exception exception)
+                        else
                         {
-                            Console.Error.WriteLine($"An error occured: {exception}");
+                            const string inputFile = "INPUT.TXT";
+                            const string outputFile = "OUTPUT.TXT";
+
+                            string dataDirectory = Environment.GetEnvironmentVariable("LAB_PATH");
+                            if (string.IsNullOrEmpty(dataDirectory))
+                            {
+                                Env.Load(".env");
+                                dataDirectory = Environment.GetEnvironmentVariable("LAB_PATH");
+
+                                if (string.IsNullOrEmpty(dataDirectory))
+                                    dataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                            }
+
+                            string inputPath = Path.Join(dataDirectory, inputFile);
+                            string outputPath = Path.Join(dataDirectory, outputFile);
+                            Lab2.WriteAnswer(inputPath, outputPath);
                         }
                     }
+                    catch (Exception exception)
+                    {
+                        Console.Error.WriteLine($"An error occured: {exception}");
+                    }
                 }
+            }
+
+            [Command("lab3", Description = "Run the lab work 3"),
+             HelpOption,
+             Subcommand(typeof(SetPath))]
+            private class RunLab3
+            {
+                [Option(ShortName = "I", Description = "Input file")]
+                public string Input { get; }
+
+                [Option(ShortName = "o", Description = "Output file")]
+                public string Output { get; }
+
+                private void OnExecute()
+                {
+                    try
+                    {
+                        if (!string.IsNullOrEmpty(Input) && !string.IsNullOrEmpty(Output))
+                        {
+                            Lab3.WriteAnswer(Input, Output);
+                        }
+                        else
+                        {
+                            const string inputFile = "INPUT.TXT";
+                            const string outputFile = "OUTPUT.TXT";
+
+                            string dataDirectory = Environment.GetEnvironmentVariable("LAB_PATH");
+                            if (string.IsNullOrEmpty(dataDirectory))
+                            {
+                                Env.Load(".env");
+                                dataDirectory = Environment.GetEnvironmentVariable("LAB_PATH");
+
+                                if (string.IsNullOrEmpty(dataDirectory))
+                                    dataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                            }
+
+                            string inputPath = Path.Join(dataDirectory, inputFile);
+                            string outputPath = Path.Join(dataDirectory, outputFile);
+                            Lab3.WriteAnswer(inputPath, outputPath);
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.Error.WriteLine($"An error occured: {exception}");
+                    }
+                }
+            }
+        }
+
+        [Command("set-path", Description = "Set LAB_PATH environment value in .env file")]
+        private class SetPath
+        {
+            [Option("-p|--path", Description = "Input and output files directory")]
+            public string ResultPath { get; }
+
+            private void OnExecute(IConsole console)
+            {
+                if (string.IsNullOrEmpty(ResultPath))
+                    console.Error.WriteLine("Please specify path for input and output files directory.");
+                else
+                    File.AppendAllText(".env", $"LAB_PATH={ResultPath}");
             }
         }
     }
